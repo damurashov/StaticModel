44f0831fa5a236b50bd842407acf2457a8105393
DmitryMurashov <dmtr.murashov@gmail.com>
2022-01-10 21:23:21 +0300

    [ex:test_output] Impl | Eq. constraint matrix output

--- ex/test_output.py
+++ ex/test_output.py
@@ -9,9 +9,10 @@ import linsolv_model as lsm
 FILE = 'data.csv'
 
 if __name__ == "__main__":
-    data.Generation.file_generate(FILE)
-    builder = lsm.Builder(data.Read.readf_iter(FILE))
+	data.Generation.file_generate(FILE)
+	builder = lsm.Builder(data.Read.readf_iter(FILE))
 
-    print(builder.mat_a)
-    lsm.Output.print_matrix(builder.mat_b.matrix)
-    print(builder.bounds)
+	print(builder.mat_a)
+	lsm.Output.print_matrix(builder.mat_b.matrix)
+	print(builder.bounds)
+	lsm.Output.print_matrix(builder.max_equ_matrix)
