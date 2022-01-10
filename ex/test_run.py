9a650e2d48e36bf3e6e0c106cb754fcb89837585
DmitryMurashov <dmtr.murashov@gmail.com>
2022-01-10 21:35:33 +0300

    [ex:test_run]

---
+++ ex/test_run.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import sys
+
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent / 'src'))
+
+import data
+import linsolv_model as lsm
+from scipy.optimize import linprog
+
+FILE = 'data.csv'
+
+if __name__ == "__main__":
+	data.Generation.file_generate(FILE)
+	builder = lsm.Builder(data.Read.readf_iter(FILE))
+
+	sol = linprog(
+		c = builder.mat_max_equation,
+		A_eq = builder.mat_a.matrix,
+		b_eq = builder.mat_b.matrix,
+		bounds = builder.bounds
+	)
+
+	print(sol)
+	print(len(sol.x))
