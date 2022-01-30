8826bc24851f7c2114e629136cfa55df4e5fc1aa
DmitryMurashov <dmtr.murashov@gmail.com>
2022-01-30 11:18:09 +0300

    [:data] Impl | KvData

--- src/data.py
+++ src/data.py
@@ -3,10 +3,130 @@
 # The indices, namely i, j, l are counted from 1. The appropriate adjustments must be taken by data consuming modules,
 # such as linsolv_model
 
-from random import randint, uniform, random
+from random import Random, randint, uniform, random
 import csv
 import re
-import argparse
+
+
+class KvData(dict):
+	"""
+	Stores key-value data pairs in format {(str(VAR), int(INDEX1), int(INDEX2), ...) : float(VALUE)}
+	"""
+	def get(self, var, *indices):
+		key = KvData._as_key(var, *indices)
+
+		if key in self.keys():
+			return self[key]
+		else:
+			self._generate(var, *indices)
+			return self[key]
+
+	@staticmethod
+	def _as_key(var, *indices):
+		assert len(indices) > 0
+		var = str(var)
+		indices = (int(i) for i in indices)
+		return (var, *indices,)
+
+	def _generate(self, key):
+		"""
+		Either generates a key guaranteeing its presence in the dictionary, or throws an assertion
+		"""
+		raise NotImplemented
+
+	def set(self, val, var, *indices):
+		val = float(val)
+		key = KvData._as_key(var, *indices)
+		self[key] = val
+
+
+class RandomKvData(KvData):
+
+	PSI_JL_MAX = 3.0  # Max channel throughput
+	V_J_MAX = 1.0  # Max memory for node J
+	PHI_JL_MAX = 1.5  # Max node performance
+	N_L = 2  # Number of structural stability intervals
+	N_J = 3  # Number of nodes
+	CONNECTEDNESS = .6  # Probability that there is a link b/w 2 nodes
+	X_JL_MAX = 2.0  # Data threshold, max constraint value. Fixed amount of data that should be processed on a given node during structural stability timespan `L`
+
+	@staticmethod
+	def _uniform(a, b):
+		f_float = type(a) is float or type(b) is float
+
+		if f_float:
+			return uniform(a, b)
+		else:
+			return randint(a ,b)
+
+	@staticmethod
+	def _is_connected() -> int:
+		return int(random() < RandomKvData.CONNECTEDNESS)
+
+	def _generate(self, key):
+
+		var = key[0]
+
+		gen_map = {
+			'v_j': lambda: RandomKvData._uniform(0, RandomKvData.V_J_MAX),
+			'phi_jl': lambda: RandomKvData._uniform(0, RandomKvData.PHI_JL_MAX),
+			'x_jl': lambda: RandomKvData._uniform(0, RandomKvData.X_JL_MAX),
+			'psi_jil': lambda: RandomKvData._is_connected() * RandomKvData._uniform(0, RandomKvData.PSI_JL_MAX),
+			'm': lambda: RandomKvData.N_J,  # Number of nodes
+			'k': lambda: RandomKvData.N_L,  # Number of structural stability intervals
+			'alpha': lambda: .5,
+		}
+		assert var in gen_map.keys()
+		assert key not in self.keys()
+		generated = gen_map[var]()
+		self.set(generated, *key)
+
+
+class UiKvData(KvData):
+
+	def _generate(self, key):
+		"""
+		Forms dictionary of values according to the following format:
+		{(str(VAR), int(INDEX1), int(INDEX2), ...) : float(VALUE)}
+		"""
+		while True:
+			if key in self.keys():
+				return
+
+			inp = input(str(key))
+			inp = inp.strip()
+			inp = re.split(r'\s+', inp)
+			cmd = inp[0] if len(inp) else ''
+
+			try:
+				if cmd == '?':
+					print('\n'.join([
+						"?  -  help",
+						"edit VAR INDEX1 INDEX2 ...  -  change entry",
+						"show  -  show all"
+					]))
+
+				elif cmd == 'show':
+					for k in self.keys():
+						print(f"{k}  -  {self[k]}")
+
+				elif cmd == 'edit':
+					k = KvData._as_key(*inp[1:])
+
+					res = self.pop(k, None)
+					print(f"Removed: {res}")
+
+					while k not in self.keys():
+						self._generate(k)
+
+				else:
+					cmd = float(cmd)
+					self.set(cmd, *key)
+
+					return
+
+			except Exception as e:
+				print(str(e))
 
 
 class Generation:
@@ -60,6 +180,10 @@ class Generation:
 					yield [l, j, i, psi_jil, v_j, phi_jl, x_jl, alpha_1, alpha_2]
 
 	@staticmethod
+	def iter_generate_kv(kv_data, f_gen_scheme = True):
+		pass
+
+	@staticmethod
 	def _ui_format_key(*args):
 		var = args[0:1]
 		inp = args[1:]
@@ -93,8 +217,6 @@ class Generation:
 				elif inp[0] == 'show':
 					for k in uimap.keys():
 						print(f"{k}  -  {uimap[k]}")
-
-					return Generation._ui_update_uimap(uimap, *args)
 				elif inp[0] == 'edit':
 					key = Generation._ui_format_key(*inp[1:])
 
@@ -108,6 +230,7 @@ class Generation:
 						inp = float(inp[0])
 						uimap[args] = inp
 
+						print(uimap)
 						return uimap
 					except Exception as e:
 						print(str(e))
