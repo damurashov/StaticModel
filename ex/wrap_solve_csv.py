a5a0df03ec116e82b1a5e3e135474d55b7df3415
DmitryMurashov <dmtr.murashov@gmail.com>
2022-02-04 20:49:05 +0300

    [ex:wrap_solve_*] Impl

---
+++ ex/wrap_solve_csv.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+import sys
+
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent / 'src'))
+
+import linsolv_model
+
+
+if __name__ == "__main__":
+    print(linsolv_model.wrap_solve_csv(sys.argv[1]))
